8dfee9bb9b77685e9d5bd00e55a13b5fb281b07d
Youngeun Jun <121297085+Junyewdd@users.noreply.github.com>
2023-07-10 01:25:14 +0900

    [FEAT] 로그인 기능 구현

--- week12/src/pages/Home.js
+++ week12/src/pages/Home.js
@@ -2,17 +2,36 @@ import React, { useState } from 'react'
 import { Input, Inputs, Title, Wrapper, Form } from '../components/Common';
 import { Link, useNavigate } from 'react-router-dom';
 import { styled } from 'styled-components';
+import { login } from '../apis/login';
 
 const Home = () => {
+    const [id, setId] = useState('');
+    const [pw, setPw] = useState('');
+    const router = useNavigate();
+    const onChangeId = (e)=>{
+        setId(e.target.value);
+    }
+    const onChangePw = (e)=>{
+        setPw(e.target.value);
+    }
+    const onClick = async()=>{
+        //로그인 api
+        const result = await login(id, pw);
+        console.log(result);
+        const {accessToken, refreshToken} = result;
+        localStorage.setItem('access', accessToken);
+        localStorage.setItem('refreshToken', refreshToken);
+        router('/mypage');
+    };
     return (
         <Wrapper>
             <Title>로그인하기</Title>
             <Form>
                 <Inputs>
-                    <Input placeholder="아이디" />
-                    <Input placeholder="비밀번호" type="password"/>
+                    <Input placeholder="아이디" value={id} onChange={onChangeId}/>
+                    <Input placeholder="비밀번호" type="password" value={pw} onChange={onChangePw}/>
                 </Inputs>
-                <Button>Login</Button>
+                <Button onClick={onClick}>Login</Button>
             </Form>
             <CustomLink to='/signup'>회원가입하기</CustomLink>
         </Wrapper>);
